3d6c457ee5da93510b574a888dec09ba611eb4f2
shreyaskargit <shreyaskar30@gmail.com>
2020-02-18 21:01:00 +0530

    uild

---
+++ build/precache-manifest.c4e702eb4a85a5210941761e7ae9ceea.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eb09de9cd376cdc2822842f3129eb515",
+    "url": "/index.html"
+  },
+  {
+    "revision": "422031f3a457dfbb5e26",
+    "url": "/static/css/main.2c8f406a.chunk.css"
+  },
+  {
+    "revision": "aab32d93a3ba44b738ac",
+    "url": "/static/js/2.1b61ca17.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/static/js/2.1b61ca17.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "422031f3a457dfbb5e26",
+    "url": "/static/js/main.069938af.chunk.js"
+  },
+  {
+    "revision": "ce665739ab12d962c470",
+    "url": "/static/js/runtime-main.95b134d0.js"
+  },
+  {
+    "revision": "ad865c2ddd07399d4d3596b333c7780b",
+    "url": "/static/media/ArcaMajora3-Bold.ad865c2d.woff2"
+  },
+  {
+    "revision": "d28f7f6a22585385c239e88ede212762",
+    "url": "/static/media/ArcaMajora3-Bold.d28f7f6a.woff"
+  },
+  {
+    "revision": "eb9478bb97f656090bbd435c50655ac2",
+    "url": "/static/media/ArcaMajora3-Bold.eb9478bb.eot"
+  },
+  {
+    "revision": "4bd33d163505e75d82bdd2ce6912bff7",
+    "url": "/static/media/ArcaMajora3-Heavy.4bd33d16.woff2"
+  },
+  {
+    "revision": "5389e2d76b470ed38a0548fd998e3389",
+    "url": "/static/media/ArcaMajora3-Heavy.5389e2d7.woff"
+  },
+  {
+    "revision": "92174473b632e38933ae646670ed8411",
+    "url": "/static/media/ArcaMajora3-Heavy.92174473.eot"
+  },
+  {
+    "revision": "200e187ef3b07259b9b8195a621f7396",
+    "url": "/static/media/Milk.200e187e.woff2"
+  },
+  {
+    "revision": "6e94e3eaf09f907564d495f9134829dc",
+    "url": "/static/media/Milk.6e94e3ea.eot"
+  },
+  {
+    "revision": "82f41ce69accc7371c9c9f80c3eadf17",
+    "url": "/static/media/Milk.82f41ce6.woff"
+  },
+  {
+    "revision": "f8c5bc63820a6fede542c3957394f811",
+    "url": "/static/media/SOUND_IN_KB.f8c5bc63.mp3"
+  },
+  {
+    "revision": "88d3f599895f785a310fcf3b7b14a1ed",
+    "url": "/static/media/backgrd image-final-1920 1080.88d3f599.jpg"
+  },
+  {
+    "revision": "978dc853c95f93fd2f201f8fa3ec4bae",
+    "url": "/static/media/loader_final.978dc853.gif"
+  },
+  {
+    "revision": "17a21cbc432cdbff16b23d8c2cded822",
+    "url": "/static/media/mobile_view_background.17a21cbc.jpg"
+  },
+  {
+    "revision": "b7c5d0807a583f2cdddc8f1b2caab619",
+    "url": "/static/media/treasuro logo final.b7c5d080.png"
+  }
+]);
